2a8b6c5e77393297ae171ee379855baf602881b0
sanevillain <denis.ismail@code.berlin>
2022-04-04 17:00:45 +0200

    Add pages directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { Route } from 'wouter';
+import { Flex } from '@chakra-ui/react';
+
+import { Navbar } from './components/navbar/Navbar';
+import HomePage from './pages/home/HomePage';
+import LoginPage from './pages/login/LoginPage';
+import SignUpPage from './pages/signup/SignUpPage';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello there</h1>
+    <div>
+      <Flex
+        color="blue"
+        w="100vw"
+        h="100vh"
+        direction="column"
+        justifyContent="flex-start"
+        alignItems="center"
+      >
+        <Navbar />
+        <Route path="/" component={HomePage} />
+        <Route path="/signup" component={SignUpPage} />
+        <Route path="/login" component={LoginPage} />
+      </Flex>
     </div>
   );
 }
---
+++ src/pages/signup/SignUpPage.tsx
@@ -0,0 +1,7 @@
+import { SignUp } from '../../components/signup/SignUp';
+
+const SignUpPage: React.FC = () => {
+  return <SignUp />;
+};
+
+export default SignUpPage;

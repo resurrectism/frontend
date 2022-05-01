4172cc2ee1f9d7276309c76eaf41a324fe6002ca
Desislava Belokonska <32732980+desi-belokonska@users.noreply.github.com>
2022-05-01 20:15:49 +0300

    Style Container component for dark/light theme

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,22 @@ import App from './App';
 import {
   ChakraProvider,
   extendTheme,
+  ThemeComponentProps,
   withDefaultColorScheme,
 } from '@chakra-ui/react';
 
 const customTheme = extendTheme(
+  {
+    components: {
+      Container: {
+        baseStyle: (props: ThemeComponentProps) => ({
+          border: '1px solid',
+          borderColor: props.colorMode === 'dark' ? 'gray.600' : 'gray.300',
+          borderRadius: 'md',
+        }),
+      },
+    },
+  },
   withDefaultColorScheme({ colorScheme: 'purple' }),
 );
 
